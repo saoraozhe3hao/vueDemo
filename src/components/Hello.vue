fa475d8d0f2bc8945297a2137df42e367d15f7a4
hongzq <saoraozhe3hao@126.com>
2017-05-16 16:51:30 +0800

    less mixin

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -97,8 +97,11 @@ export default {
 
   <!-- scoped 使得样式只应用在当前component， 要使用less需安装less 和 less-loader-->
 <style scoped lang="less">
+// 引入less  mixin
+@import '../assets/text-overflow';
 h1, h2 {
   font-weight: normal;
+  .text-overflow();
 }
 
 ul {
